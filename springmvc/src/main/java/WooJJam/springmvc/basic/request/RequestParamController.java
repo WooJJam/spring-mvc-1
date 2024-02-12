3bd43064ea6c973906aac9e26b3c6a31d47c9df1
WooJJam <jaemin5548@naver.com>
2024-02-12 18:42:51 +0900

    [section 6] 스프링 MVC - 기본기능 (HTTP 요청 파라미터 - @RequestParam)

--- springmvc/src/main/java/WooJJam/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/WooJJam/springmvc/basic/request/RequestParamController.java
@@ -5,8 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,5 +22,70 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @RequestMapping("/request-param-v2")
+    @ResponseBody
+    public String requestParamV2(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age) throws IOException {
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-v3")
+    @ResponseBody
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) throws IOException {
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-v4")
+    @ResponseBody
+    public String requestParamV4(String username, int age) throws IOException {
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-required")
+    @ResponseBody
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) throws IOException {
+        /**
+         * required = false 로 설정하면 null이 들어감
+         * 하지만 int에는 null이 들어갈 수 없으므로 Integer로 해야함
+         */
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-default")
+    @ResponseBody
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) throws IOException {
+        /**
+         * required = false 로 설정하면 null이 들어감
+         * 하지만 int에는 null이 들어갈 수 없으므로 Integer로 해야함
+         */
+        log.info("username={}, age={}", username, age);
+
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-map")
+    @ResponseBody
+    public String requestParamMap(
+            @RequestParam Map<String, Object> paramMap) throws IOException {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+
+        return "ok";
     }
 }
